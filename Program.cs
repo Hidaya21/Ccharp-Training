6871f091da169d46e21c0b07bbe34b37f4d2020c
Hidaya21 <alabrihidaya@gmail.com>
2026-05-12 09:57:40 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BankingServices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
